310538950acca373f31d810c4a41db1fefa8e7d3
Conrad Li <zli3@bowdoin.edu>
2020-06-01 19:17:17 -0400

    first commit

---
+++ assets/js/scale.fix.js
@@ -0,0 +1,49 @@
+var metas = document.getElementsByTagName('meta');
+var i;
+if (navigator.userAgent.match(/iPhone/i)) {
+  for (i=0; i<metas.length; i++) {
+    if (metas[i].name == "viewport") {
+      metas[i].content = "width=device-width, minimum-scale=1.0, maximum-scale=1.0";
+    }
+  }
+  document.addEventListener("gesturestart", gestureStart, false);
+}
+function gestureStart() {
+  for (i=0; i<metas.length; i++) {
+    if (metas[i].name == "viewport") {
+      metas[i].content = "width=device-width, minimum-scale=0.25, maximum-scale=1.6";
+    }
+  }
+}
+
+document.getElementById("openNav").addEventListener("click", function(){
+  
+  document.getElementById('openNav').className = "fa fa-bars inactive"; 
+  
+
+  document.getElementById('closeNav').className = "fas fa-times active";
+
+  openNav();
+
+});
+
+document.getElementById("closeNav").addEventListener("click", function() {
+
+  document.getElementById('closeNav').className = "fas fa-times inactive";
+
+
+  document.getElementById('openNav').className = "fa fa-bars active"; 
+  closeNav();
+
+
+
+});
+
+function openNav() {
+  document.getElementById("myNav").style.width = "100%";
+}
+
+function closeNav() {
+  document.getElementById("myNav").style.width = "0%";
+}
+
